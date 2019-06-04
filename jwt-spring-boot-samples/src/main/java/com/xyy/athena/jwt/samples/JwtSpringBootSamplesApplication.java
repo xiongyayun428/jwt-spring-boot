0fbac21a037043ea4dc1be9031cec7c232dd1b18
xiongyayun428@163.com <Xyy42899>
2019-06-05 00:05:56 +0800

    init

--- jwt-spring-boot-samples/src/main/java/com/xyy/athena/jwt/samples/JwtSpringBootSamplesApplication.java
+++ jwt-spring-boot-samples/src/main/java/com/xyy/athena/jwt/samples/JwtSpringBootSamplesApplication.java
@@ -3,6 +3,12 @@ package com.xyy.athena.jwt.samples;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * JwtSpringBootSamplesApplication
+ *
+ * @author Yayun.Xiong
+ * @date 2019-06-04
+ */
 @SpringBootApplication
 public class JwtSpringBootSamplesApplication {
 
